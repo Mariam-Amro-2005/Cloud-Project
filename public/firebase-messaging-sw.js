c420853d94cf8b5320bd28b71fe5f0aa25c4d184
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2026-03-26 12:43:31 +0200

    feat: Implement a real-time notification system using Firebase Cloud Messaging and Realtime Database, including a user interface.

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,42 @@
+importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-database-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDODNh-BFWI0-QD5jcfSSKTWhh8T91Rr54",
+  authDomain: "cloud-project-2026-bcb55.firebaseapp.com",
+  databaseURL: "https://cloud-project-2026-bcb55-default-rtdb.firebaseio.com",
+  projectId: "cloud-project-2026-bcb55",
+  storageBucket: "cloud-project-2026-bcb55.firebasestorage.app",
+  messagingSenderId: "679872158612",
+  appId: "1:679872158612:web:d0015e9e265b153d1b0d81",
+});
+
+const messaging = firebase.messaging();
+const db = firebase.database();
+let username = "guest";
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
+  const notificationTitle = payload.notification?.title || payload.data?.title || 'New Notification';
+  const notificationBody = payload.notification?.body || payload.data?.body || '';
+
+  const notificationOptions = {
+    body: notificationBody,
+    icon: '/favicon.ico'
+  };
+  const username = payload.data?.username || "guest";
+  console.log("Username: ", username);
+  console.log(payload.data)
+
+  db.ref(`notifications/${username}`).push({
+    title: notificationTitle,
+    body: notificationBody,
+    receivedAt: Date.now()
+  }).catch((error) => {
+    console.error('Failed to update DB in background:', error);
+  });
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
